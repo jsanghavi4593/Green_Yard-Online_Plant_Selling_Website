8710ff862ebfa1b4698b3d59869929482f89e277
Jainil Sanghavi <82511070+jsanghavi4593@users.noreply.github.com>
2021-05-04 16:52:31 +0530

    Add files via upload

---
+++ server.php
@@ -0,0 +1,69 @@
+<?php
+
+
+
+$username    = "";
+$email    = "";
+$password    = "";
+$phone = "";
+$age   = "";
+$gender="";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '','joblister');
+
+// REGISTER USER
+if (isset($_POST['submit']))
+ {
+  // receive all input values from the form
+ 
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+   $password = mysqli_real_escape_string($db, $_POST['password']);
+    $phone = mysqli_real_escape_string($db, $_POST['phonenumber']);
+     $age = mysqli_real_escape_string($db, $_POST['age']);
+     $gender=mysqli_real_escape_string($db, $_POST['gender']);
+
+  
+
+  
+  $user_check_query = "SELECT * FROM admin WHERE email='$email' OR username='$username' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { 
+
+
+     if ($user['username'] == $username) {
+      array_push($errors, "username already exists");
+       echo"username already exists";
+    }
+   
+    if ($user['email'] == $email) {
+      array_push($errors, "email already exists");
+       echo"email already exists";
+    }
+
+   
+  }
+
+   if (count($errors) == 0)
+   {
+
+   
+         $query = "INSERT INTO admin (username,email,password,phonenumber,age,gender) 
+          VALUES('$username','$email','$password','$phone','$age','$gender')";
+          mysqli_query($db, $query);
+      
+      } 
+
+      else {
+        echo "Sorry, there was an error";
+      }
+
+  }
+
+?>
+
+ 
